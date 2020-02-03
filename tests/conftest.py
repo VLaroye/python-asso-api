af4b6f7becffc94abc57eb67daa13fc1034477d7
VLaroye <laroye.vincent@gmail.com>
2020-02-03 11:59:56 +0100

    Finished 'list_accounts' test

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,9 @@ import pytest
 from vasso import create_app, db as _db
 from vasso.config import TestingConfig
 
+from vasso.auth.models import User
+from vasso.accounts.models import Account
+
 
 @pytest.fixture
 def app():
@@ -15,6 +18,15 @@ def db(app):
     _db.app = app
     _db.drop_all()
     _db.create_all()
+
+    # Insert fake data
+    user1 = User(username="User1", email="user1@email.com")
+    account1 = Account(name='Account 1', created_by='1')
+
+    _db.session.add(user1)
+    _db.session.add(account1)
+    _db.session.commit()
+
     return _db
 
 
--- tests/functionnal/test_accounts.py
+++ tests/functionnal/test_accounts.py
@@ -1,3 +1,6 @@
+import json
+
+
 def test_list_accounts(client, db):
     """
     GIVEN a Flask application
@@ -6,6 +9,14 @@ def test_list_accounts(client, db):
     """
     response = client.get('/accounts/')
 
-    print(response)
+    data = json.loads(response.data.decode())
+
+    account = data['data'][0]
+    status = data['status']
+
     assert response.status_code == 200
+    assert status
+    assert account['name'] == 'Account 1'
+    assert account['id'] == 1
+    assert account['creator'] == 1
 
